188807f26f9b0d599e4578a23b158b177d88092f
Maksym Portianoi <maksymportianoi@Maksyms-MacBook-Air.local>
2021-02-25 17:04:23 -0500

    add concat functionality

--- audio-stash/handler.js
+++ audio-stash/handler.js
@@ -1,9 +1,22 @@
-
 const { spawnSync } = require('child_process')
-const { readFileSync, writeFileSync, unlinkSync } = require('fs')
+const { readFileSync, writeFileSync, unlinkSync, writeFile } = require('fs')
 const AWS = require('aws-sdk')
+const { promisify } = require('util')
 
 const s3 = new AWS.S3()
+const asyncWriteFile = promisify(writeFile)
+
+function checkS3 (key) {
+  return new Promise((resolve, reject) => {
+    s3.headObject({ Bucket: process.env.OUTPUT_BUCKET, Key: key }, (err, metadata) => {
+      if (err && ['NotFound', 'Forbidden'].indexOf(err.code) > -1) return resolve()
+      else if (err)
+        return reject(err)
+
+      return resolve(metadata)
+    })
+  })
+}
 
 module.exports.trimConcatStash = async (event, context) => {
   const eventBody = JSON.parse(event.body)
@@ -21,19 +34,20 @@ module.exports.trimConcatStash = async (event, context) => {
   let trimOptions = {}
   if (needsTrimming) trimOptions = { start: eventBody.trim_start, length: eventBody.trim_length }
 
+  console.log('getting the file')
   // get the file
-  const Os3Object = await s3
+  const s3Object = await s3
     .getObject({
-      Bucket: 'outil-input-audio',
+      Bucket: process.env.INPUT_BUCKET,
       Key: fileName
     })
     .promise()
 
   // write file to tmp folder
-  writeFileSync(needsTrimming ? '/tmp/original.mp3' : '/tmp/output.mp3', Os3Object.Body)
+  writeFileSync(needsTrimming ? '/tmp/original.mp3' : '/tmp/output.mp3', s3Object.Body)
 
   if (needsTrimming)
-  // crop
+    // crop
     spawnSync(
       '/opt/ffmpeg/ffmpeg',
       [
@@ -52,78 +66,79 @@ module.exports.trimConcatStash = async (event, context) => {
 
   // delete the temp files
   if (needsTrimming) unlinkSync('/tmp/original.mp3')
-  let resultFile = '/tmp/output.mp3'
 
-  try {
-    // check if recording already exists
-    const objectExists = await s3.headObject({
-      Bucket: 'outil-output-audio',
-      Key: fileName
-    }).promise()
+  let resultFilePath = '/tmp/output.mp3'
 
-    if (objectExists) {
-      // get the file
-      const Pt1s3Object = await s3
-        .getObject({
-          Bucket: 'outil-output-audio',
-          Key: fileName
-        })
-        .promise()
+  // check if recording already exists
+  const needsConcat = await checkS3(fileName)
 
-      // write file to tmp folder
-      writeFileSync('/tmp/pt1.mp3', Pt1s3Object.Body)
-
-      // concat with existing recording
-      spawnSync(
-        '/opt/ffmpeg/ffmpeg',
-        [
-          '-i',
-          '"concat:/tmp/pt1.mp3|/tmp/output.mp3"',
-          '-acodec',
-          'copy',
-                  `/tmp/${fileName}`
-        ],
-        { stdio: 'inherit' }
-      )
-
-      resultFile = `/tmp/${fileName}`
-
-      // delete the temp files
-      unlinkSync('/tmp/pt1.mp3')
-      unlinkSync('/tmp/output.mp3')
-
-      // delete previous recording from output bucket
-      await s3.deleteObject({
-        Bucket: 'outil-output-audio',
+  if (needsConcat) {
+    console.log('getting pt1 file')
+    // get pt1 file
+    const Pt1s3Object = await s3
+      .getObject({
+        Bucket: process.env.OUTPUT_BUCKET,
         Key: fileName
-      }).promise()
-    }
-  } catch (err) {
-    console.log('error checking for existing file', err)
+      })
+      .promise()
+
+    // write file to tmp folder
+    writeFileSync('/tmp/pt1.mp3', Pt1s3Object.Body)
+
+    console.log('creating txt file')
+    // create txt file
+    await asyncWriteFile('/tmp/list.txt', 'file \'/tmp/pt1.mp3\'\r\nfile \'/tmp/output.mp3\'', (err) => {
+      if (err)
+        console.log(err)
+    })
+
+    // concat
+    spawnSync(
+      '/opt/ffmpeg/ffmpeg',
+      [
+        '-f',
+        'concat',
+        '-safe',
+        '0',
+        '-i',
+        '/tmp/list.txt',
+        '-c',
+        'copy',
+                  `/tmp/${fileName}`
+      ],
+      { stdio: 'inherit' }
+    )
+
+    resultFilePath = `/tmp/${fileName}`
+
+    console.log('deleting the temp files')
+    // delete the temp files
+    unlinkSync('/tmp/pt1.mp3')
+    unlinkSync('/tmp/output.mp3')
   }
 
   // read file
-  const file = readFileSync(resultFile)
-  unlinkSync(resultFile)
+  const file = readFileSync(resultFilePath)
+  // delete the temp files
+  unlinkSync(resultFilePath)
 
+  console.log('uploading new file to s3')
   // upload new file to s3
-  await s3
-    .putObject({
-      Bucket: 'outil-output-audio',
-      Key: fileName,
-      Body: file
-    })
-    .promise()
-    .then(async () => {
-      // delete original object from input bucket
-      await s3.deleteObject({
-        Bucket: 'outil-input-audio',
-        Key: fileName
-      }).promise()
+  await s3.putObject({
+    Bucket: process.env.OUTPUT_BUCKET,
+    Key: fileName,
+    Body: file
+  }).promise()
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'success' })
-      }
-    })
+  console.log('deleting original object from input bucket (wip)')
+  // delete original object from input bucket
+  await s3.deleteObject({
+    Bucket: process.env.INPUT_BUCKET,
+    Key: fileName
+  }).promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'success' })
+  }
 }
