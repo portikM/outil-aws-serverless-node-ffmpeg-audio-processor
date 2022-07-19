32da6bbe0203cf429d178b28de75aaea0f1e6ee0
Maksym Portianoi <portimaksym@gmail.com>
2022-07-18 23:22:49 -0400

    add trim feature

--- src/infrastructure/storage/storage.service.ts
+++ src/infrastructure/storage/storage.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
-const fs = require('fs').promises;
+import { MediaTypesEnum } from '../../common/enums/media-types.enum';
+import { promises as fs } from 'fs';
 
 @Injectable()
 export class StorageService {
@@ -10,7 +11,7 @@ export class StorageService {
 
   getSignedURL(mediaType: string, key: string): string {
     const s3 = this.getS3();
-    const extension = mediaType === 'audio' ? '.mp3' : '.mp4';
+    const extension = mediaType === MediaTypesEnum.AUDIO ? '.mp3' : '.mp4';
     return s3.getSignedUrl('putObject', {
       Bucket: process.env.INPUT_BUCKET,
       Key: `${mediaType}/${key + extension}`,
@@ -19,7 +20,7 @@ export class StorageService {
 
   getObject(mediaType: string, key: string) {
     const s3 = this.getS3();
-    const extension = mediaType === 'audio' ? '.mp3' : '.mp4';
+    const extension = mediaType === MediaTypesEnum.AUDIO ? '.mp3' : '.mp4';
     return new Promise((resolve, reject) => {
       s3.getObject(
         {
@@ -39,15 +40,22 @@ export class StorageService {
 
   async putObject(mediaType: string, key: string, path: string) {
     const s3 = this.getS3();
-    const extension = mediaType === 'audio' ? '.mp3' : '.mp4';
+    const extension = mediaType === MediaTypesEnum.AUDIO ? '.mp3' : '.mp4';
+    const contentType =
+      mediaType === MediaTypesEnum.AUDIO ? 'audio/mpeg' : 'video/mp4';
     const body = await fs.readFile(path);
+
+    if (!body) {
+      throw new NotFoundException('Audio file not found in temp folder');
+    }
+
     return new Promise((resolve, reject) => {
       s3.putObject(
         {
-          Bucket: process.env.INPUT_BUCKET as string,
-          Key: `${mediaType}-output/${key + extension}`,
+          Bucket: process.env.OUTPUT_BUCKET as string,
+          Key: `${mediaType}/${key + extension}`,
           Body: body,
-          ContentType: 'audio/mpeg',
+          ContentType: contentType,
           ACL: 'public-read',
         },
         (err, data) => {
--- src/trim/trim.service.ts
+++ src/trim/trim.service.ts
@@ -7,7 +7,8 @@ import {
 import { StorageService } from '../infrastructure/storage/storage.service';
 import { TrimAudioDto } from './dto/trim-audio.dto';
 import { MediaTypesEnum } from '../common/enums/media-types.enum';
-const fs = require('fs').promises;
+import { promises as fs } from 'fs';
+const { spawnSync } = require('child_process');
 
 @Injectable()
 export class TrimService {
@@ -25,6 +26,7 @@ export class TrimService {
       if (error.statusCode && error.statusCode === 404) {
         throw new NotFoundException('Audio file not found');
       } else {
+        console.log(error);
         throw new InternalServerErrorException(
           'Error getting audio file from storage',
         );
@@ -32,10 +34,40 @@ export class TrimService {
     }
 
     // save file to temp folder
-    await fs.writeFile('/tmp/original.mp3', file);
+    await fs.writeFile(`/tmp/${trimAudioDto.key}.mp3`, file as Buffer);
 
-    // TODO: trim the audio file and save to storage
+    // if file needs to be trimmed, trim it.. (c) Jason Statham
+    let outputFile = `/tmp/${trimAudioDto.key}.mp3`;
+    const needsTrimming =
+      trimAudioDto.trimStart !== undefined &&
+      trimAudioDto.trimLength !== undefined;
+    if (needsTrimming) {
+      outputFile = '/tmp/output.mp3';
 
-    return '';
+      spawnSync(
+        '/opt/bin/ffmpeg',
+        [
+          '-ss',
+          `${trimAudioDto.trimStart}`,
+          '-t',
+          `${trimAudioDto.trimLength}`,
+          '-i',
+          `/tmp/${trimAudioDto.key}.mp3`,
+          '-acodec',
+          'copy',
+          outputFile,
+        ],
+        { stdio: 'inherit' },
+      );
+    }
+
+    // upload audio file to storage
+    await this.storageService.putObject(
+      MediaTypesEnum.AUDIO,
+      trimAudioDto.key,
+      outputFile,
+    );
+
+    return `https://${process.env.OUTPUT_BUCKET}.s3.ca-central-1.amazonaws.com/audio/${trimAudioDto.key}.mp3`;
   }
 }
